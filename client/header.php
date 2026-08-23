97069cb2539aedbbc3d86c0fda6bdea1ca26c84a
lifelinecoding <apatel89329@gmail.com>
2026-08-23 19:38:40 +0530

    Project setup

---
+++ client/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Category</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Latest Questions</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
